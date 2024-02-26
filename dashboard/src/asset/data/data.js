fe87508b2085fa4af0f45db72fbde2af122df101
Hoang Anh <anhvh1412@gmail.com>
2024-02-26 17:05:52 +0700

    first commit

---
+++ dashboard/src/asset/data/data.js
@@ -0,0 +1,38 @@
+[
+    {
+        "subjectMa": "CT001",
+        "subjectTen": "Vi tích phân",
+        "subjectSotc": "3"
+    },
+    {
+        "subjectMa": "CT002",
+        "subjectTen": "Cơ sở dữ liệu",
+        "subjectSotc": "1"
+    },
+    {
+        "subjectMa": "CT003",
+        "subjectTen": "Nhập môn lập trình web",
+        "subjectSotc": "2"
+    },
+    {
+        "subjectMa": "CT004",
+        "subjectTen": "Quản trị hệ thống",
+        "subjectSotc": "2"
+    },
+    {
+        "subjectMa": "CT005",
+        "subjectTen": "Đảm bảo chất lượng và kiểm thử phần mềm",
+        "subjectSotc": "1"
+    },
+    {
+        "subjectMa": "CT006",
+        "subjectTen": "Kiến trúc và thiết kế phần mềm",
+        "subjectSotc": "3"
+    },
+    {
+        "subjectMa": "CT007",
+        "subjectTen": "Quản lý dự án phần mềm",
+        "subjectSotc": "3"
+    },
+
+]
